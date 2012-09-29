e516c80d8752f698896724111c69eba0f162e54a
Abhishek <abhishek.es@mail.com>
2012-09-29 16:23:26 -0500

    Changes in marshaling

--- commons/message_type.h
+++ commons/message_type.h
@@ -6,6 +6,11 @@
 #include "time.h"
 #define FILE_PATH_LENGTH 200
 #define MAX_ELEMENTS_PER_PAYLOAD 8
+
+/*Macros used for Marshalling */
+#define marshal_field_info(data_structure, field) \
+    (int)(&(((data_structure *)0)->field)), sizeof(((data_structure *)0)->field)
+
 typedef enum {
    MSG_HEARTBEAT = 0,
    MSG_ADD_DELETE_NODE,
@@ -50,10 +55,13 @@ typedef struct
 {
   uint8_t numOfNodes;
   uint8_t flags;
-  #define ADD_PAYLOAD       0x01
-  #define DELETE_PAYLOAD    0x02
-  #define DELTA_PAYLOAD     0x04                      //This bit is set if only the new node in the system is getting added  
-  #define COMPLETE_PAYLOAD  0x08                      //This bit is set if set of all nodes is being sent. 
+  uint8_t ttl;                                            //Indicates the number of nodes this message has to be forwarded to
+  #define ADD_PAYLOAD           0x01
+  #define DELETE_PAYLOAD        0x02
+  #define LEAVE_NOTIFICATION    0x04
+  #define NODE_FAILURE          0x08 
+  #define DELTA_PAYLOAD         0x10                      //This bit is set if only the new node in the system is getting added  
+  #define COMPLETE_PAYLOAD      0x20                      //This bit is set if set of all nodes is being sent. 
   char    ipAddr[0][16];                             
 }addDeleteNodePayload;
 
